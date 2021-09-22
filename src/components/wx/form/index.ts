c07474e676a4edd00306d7484623a39b71068643
xlearns <62782792+xlearns@users.noreply.github.com>
2021-09-22 18:53:35 +0800

    Delete src/components/wx directory

--- src/components/wx/form/index.ts
+++
@@ -1,7 +0,0 @@
-import WxForm from '@/components/wx/form/wxForm.vue';
-import WxInput from '@/components/wx/form/input/wxInput.vue';
-import WxFormItem from '@/components/wx/form/item/wxFormItem.vue';
-
-export  const wxForm  = WxForm
-export  const wxInput  = WxInput
-export  const wxFormItem  = WxFormItem
